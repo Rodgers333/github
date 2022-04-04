7c3881f1be057dd2b07094beb5a0ff91dd07cf64
Rodgers333 <102975802+Rodgers333@users.noreply.github.com>
2022-04-04 08:30:21 -0600

    added a and b buttons

--- main.ts
+++ main.ts
@@ -1,3 +1,9 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Duck)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showIcon(IconNames.Skull)
+})
 basic.showString("Hello!")
 basic.clearScreen()
 basic.forever(function () {
